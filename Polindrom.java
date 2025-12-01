13e90a2760f9e236aacc9452925901ed6a294381
Tamar-BenChaim <tamar0556752279@gmail.com>
2025-12-01 22:11:55 +0200

    Update Polindrom.java

--- Polindrom.java
+++ Polindrom.java
@@ -1,57 +1,59 @@
 package aaa;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
-public class Set {
-	
-	
-	public static int set(int arr[])
-	{
-		int [] count = new int [arr.length];
-		int max ;
-		count[0]=1;
-		for (int i = 0; i < arr.length; i++) 
-		{
-			max = 0;
-			for (int j = 0; j < i; j++) 
-			{
-				if(arr[i] > arr[j] && count[j]>max) 
-					max = count[j];		
+public class polindrom {
+
+	public static int polindrom(String str) {
+		int mat [] []=new int[str.length()+1][str.length()+1];
+		for (int i = 1; i < mat.length; i++) {
+			mat[i][i] = 1;
+		}
+		for (int len = 1; len < mat.length; len++) {
+			for (int i = 1; i < mat.length-len; i++) {
+				int j = i + len;
+				mat[i][j]=Math.max(mat[i][j-1], mat[i+1][j]);
+				if(str.charAt(i-1) == str.charAt(j-1))
+					mat[i][j] = Math.max(mat[i][j], mat[i+1][j-1]+2);
 			}
+		}
+		for (int i = 0; i < mat.length; i++) 
+		{
+			System.out.println(Arrays.toString(mat[i]));
+		}
+		
+		int i = 1;
+		int j = mat.length-1;
+		while(i>0 && j>0) 
+		{
+			if(mat[i][j-1] == mat[i][j])
+			{
+				j = j-1;
+			}
+			else if(mat[i+1][j] == mat[i][j]) 
+			{
+				System.out.print(str.charAt(j-1));
+				i = i+1;
+			}
+			else if(mat[i+1][j-1]+2 == mat[i][j]) 
+			{
+				System.out.print(str.charAt(j-1));
+				i = i+1;
+				j = j-1;
+			}			
 			
-			count[i] = max+1;
-		}
-		System.out.println(Arrays.toString(count));
-		max = 0;
-		int imax = 0;
-		for (int i = 0; i < count.length; i++) {
-			if(count[i] > max) {
-				max = count[i];
-				imax = i;
-			}	
-		}
-		for (int i = imax ; i >= 0;) 
-		{
-			System.out.print(arr[i]+",");
-			if(i == 0)
-				break;
-			for (int j = i; j >= 0; j--) 
-			{
-				if( arr[i] > arr[j] && count[j] == count[i]-1 ) {
-					i = j;
-					break;
-				}
-			}
 		}
 		System.out.println();
-		return max;
+		return mat[1][mat.length-1];
 	}
-
-	public static void main(String[] args) 
-	{
-		int arr [] = {3,4,7,5,6,8,2,3,8,9,4,6};
-		System.out.println(set(arr));
+	
+	
+	public static void main(String[] args) {
+		String str="srrtgrssgr";
+		System.out.println(polindrom(str));
 
 	}
 
 }
+
